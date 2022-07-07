039c700c6e897f0b4545327b5962eaf5ac67bc69
Manav <51614345+plutoniumm@users.noreply.github.com>
2022-07-07 17:37:26 +0530

    Cleanup

--- local/references.js
+++
@@ -1,8 +0,0 @@
-const schema = {
-    "requestresponse": {
-        status: "LIVE",
-        message: "You are signed in as {username}",
-        logoutmessage: "You have successfully logged off",
-        state: {}
-    }
-};
--- todo/CLI2UI.js
+++
@@ -1,8 +0,0 @@
-export const CLI2UI = {
-    ytd: {
-        cmd: ( url ) => `youtube-dl ${ url } --no-check-certificate`,
-    },
-    ig: {
-        cmd: ( url, usr, pwd ) => `instagram-scraper ${ url } -u ${ usr } -p ${ pwd }`,
-    }
-}
